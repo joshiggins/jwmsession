3ff86894274e6b1b30375f940edc85249e58a130
Joshua Higgins <joshiggins@gmail.com>
2014-03-23 11:09:47 +0000

    rcgen.py: Removed unused import

--- src/jwmsession/rcgen.py
+++ src/jwmsession/rcgen.py
@@ -26,8 +26,6 @@ You should have received a copy of the GNU General Public License
 along with KX Platform. If not, see http://www.gnu.org/licenses/.
 '''
 
-import os
-
 
 values = ['menufile',
           'tray-y',
